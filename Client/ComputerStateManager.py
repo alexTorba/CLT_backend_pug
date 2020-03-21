a8af0fd8cfe3f6bfcf770a6d71d2798d8600ac40
Vladislav Smirnov <bf3le.2012@gmail.com>
2020-03-21 21:13:25 +0200

    + added saving temporary data into local file on client side

--- Client/ComputerStateManager.py
+++ Client/ComputerStateManager.py
@@ -1,28 +1,47 @@
 import psutil
 import time
+import os
 
 from Common.Entities.ComputerState import ComputerState
 from Common.Entities.ComputerFlow import ComputerFlow
+from External.JsonFomatterModule.JsonFormatter import JsonFormatter
 
 
 class ComputerStateManager:
-
-    def __init__(self):
-        self.__computer_flow = ComputerFlow()
+    __temp_data_file = "temp_computer_flow.dat"
 
     def send_data_to_server(self) -> None:
-        self.__computer_flow = ComputerFlow()
-        pass
+        computer_flow = self.__read_temp_data()
+        # TODO send
+        os.remove(self.__temp_data_file)
 
     def save_current_state(self) -> None:
         current_state = self.__read_current_computer_state()
-        self.__computer_flow.store_computer_state(current_state)
 
-    def __read_current_computer_state(self) -> ComputerState:
+        computer_flow = self.__read_temp_data()
+        computer_flow.store_computer_state(current_state)
+        self.__store_temp_data(computer_flow)
+
+    @classmethod
+    def __read_temp_data(cls) -> ComputerFlow:
+        if os.path.exists(cls.__temp_data_file):
+            with open(cls.__temp_data_file, "r") as file:
+                temp_data = file.read()
+            return JsonFormatter.deserialize(temp_data, ComputerFlow)
+        else:
+            return ComputerFlow()
+
+    @classmethod
+    def __store_temp_data(cls, data) -> None:
+        with open(cls.__temp_data_file, "w") as temp_data_file:
+            temp_data_file.write(JsonFormatter.serialize(data))
+
+    @classmethod
+    def __read_current_computer_state(cls) -> ComputerState:
         current_time = time.time()
         cpu_usage = psutil.cpu_percent()
-        ram_usage = self.__read_memory_usage()
-        disk_usage = self.__read_disk_usage()
+        ram_usage = cls.__read_memory_usage()
+        disk_usage = cls.__read_disk_usage()
         return ComputerState(current_time, cpu_usage, ram_usage, disk_usage)
 
     @staticmethod
