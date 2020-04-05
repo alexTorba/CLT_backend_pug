027e8120e7652aa81fc42885d96abbd6aae9c3ac
Vladislav Smirnov <bf3le.2012@gmail.com>
2020-04-05 15:14:47 +0300

    - removed test code

--- Client/Application.py
+++ Client/Application.py
@@ -13,21 +13,14 @@ class Application:
     def run(self) -> None:
         self.__config_manager.start_receiving()
 
-        x = 0  # for testing
-
         last_time_sent_data = time.time()
         while True:
             self.__computer_state_manager.save_current_state()
 
             current_config = self.__config_manager.get_current_state()
-            # self.__computer_state_manager.send_data_to_server()  # to check server method
 
             if last_time_sent_data + current_config.send_data_period < time.time():
                 self.__computer_state_manager.send_data_to_server()
                 last_time_sent_data = time.time()
-
-            x += 1  # for testing
-            if x > 4:  # for testing
-                break  # for testing
 
             time.sleep(current_config.check_state_period)
